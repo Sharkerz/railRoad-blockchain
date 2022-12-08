85e2386d2f0f5ca58367c96f265631219a029779
Maxence <maxence.lux@supinfo.com>
2022-12-08 16:08:31 +0100

    fix

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -1,11 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
 const railRoad = artifacts.require("RailRoad");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-
   deployer.deploy(railRoad, "0xedb20A0fD1F72C0aE0D8953EdB94BB8c0Cc0b009", 2);
 };
